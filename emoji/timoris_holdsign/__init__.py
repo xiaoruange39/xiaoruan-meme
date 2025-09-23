593f0aa05559105d7b13d0e0c099b3d6cb1504a2
xiaoruange39 <2234884717@qq.com>
2025-09-23 16:13:06 +0800

    Add files via upload

---
+++ emoji/timoris_holdsign/__init__.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.exception import TextOverLength
+
+img_dir = Path(__file__).parent / "images"
+
+
+def raise_sign(images, texts: list[str], args):
+    text = texts[0]
+    frame = BuildImage.open(img_dir / "0.png")
+    text_img = BuildImage.new("RGBA", (360, 290))
+    try:
+        text_img.draw_text(
+            (10, 10, 350, 290),
+            text,
+            max_fontsize=80,
+            min_fontsize=30,
+            allow_wrap=True,
+            lines_align="center",
+            font_families=["FZShaoEr-M11S"],
+            fill="#51201b",
+        )
+    except ValueError:
+        raise TextOverLength(text)
+    text_img = text_img.perspective(((10, -35), (395, 30), (355, 325), (-40, 250)))
+    frame.paste(text_img, (280, 555), alpha=True)
+    return frame.save_jpg()
+
+
+add_meme(
+    "timoris_sign",
+    raise_sign,
+    min_texts=1,
+    max_texts=1,
+    default_texts=["站街,不卖纯馋人"],
+    keywords=["八幡海铃举牌","海铃举牌"],
+    date_created=datetime(2022, 6, 12),
+    date_modified=datetime(2023, 2, 14),
+)
