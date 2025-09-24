0d062c5f8d4a9bf1f014a5f971b14fb9c4c9be60
xiaoruange39 <2234884717@qq.com>
2025-09-24 08:27:01 +0800

    Update __init__.py

--- emoji/sakiko_holdsign/__init__.py
+++ emoji/sakiko_holdsign/__init__.py
@@ -38,6 +38,6 @@ add_meme(
     max_texts=1,
     default_texts=["卖,给钱就做"],
     keywords=["丰川祥子举牌","祥子举牌","小祥举牌","举牌"],
-    date_created=datetime(2022, 6, 12),
-    date_modified=datetime(2023, 2, 14),
+    date_created=datetime(2025, 9, 23),
+    date_modified=datetime(2025, 9, 23),
 )
