c2f17c7a0ddfa116cecbe0e480dd55cc4d4c50ef
xiaoruange39 <2234884717@qq.com>
2025-09-25 14:44:31 +0800

    添加新表情：姬野星奏举牌/星奏举牌

---
+++ emoji/sena_holdsign/__init__.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.exception import TextOverLength
+from meme_generator.tags import MemeTags
+
+img_dir = Path(__file__).parent / "images"
+
+
+def sena_holdsign(images, texts: list[str], args):
+    text = texts[0]
+    frame = BuildImage.open(img_dir / "0.png")
+    try:
+        frame.draw_text(
+            (279, 817, 567, 1040),
+            text,
+            fill=(0, 0, 0),
+            allow_wrap=True,
+            max_fontsize=120,
+            min_fontsize=30,
+            lines_align="center",
+            font_families=["FZShaoEr-M11S"],
+        )
+    except ValueError:
+        raise TextOverLength(text)
+    return frame.save_jpg()
+
+
+add_meme(
+    "sena_holdsign",
+    sena_holdsign,
+    min_texts=1,
+    max_texts=1,
+    default_texts=["不卖,纯馋人"],
+    keywords=["姬野星奏举牌", "星奏举牌"],
+    tags=MemeTags.wuthering_waves,
+    date_created=datetime(2025, 9, 25),
+    date_modified=datetime(2025, 9, 25),
+)
