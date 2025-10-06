212555a6b60f391ed3a345cee7baad6e6fa97e7d
xiaoruange39 <2234884717@qq.com>
2025-10-06 17:19:02 +0800

    ➕添加新表情：小祥喜欢/丰川祥子喜欢/祥子喜欢

---
+++ emoji/saki_like/__init__.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.exception import TextOverLength
+from meme_generator.utils import make_jpg_or_gif
+
+img_dir = Path(__file__).parent / "images"
+
+default_text = "祥子喜欢这个"
+
+
+def saki_like(images: list[BuildImage], texts: list[str], args):
+    text = texts[0] if texts else default_text
+    frame = BuildImage.open(img_dir / "0.png")
+    try:
+        frame.draw_text(
+            (20, frame.height - 130, frame.width - 20, frame.height),
+            text,
+            max_fontsize=70,
+            min_fontsize=25,
+            stroke_fill="white",
+            stroke_ratio=0.06,
+        )
+    except ValueError:
+        raise TextOverLength(text)
+
+    def make(imgs: list[BuildImage]) -> BuildImage:
+        points = ((20, -93), (385, 51), (385, 533), (20, 709))
+        screen = (
+            imgs[0]
+            .convert("RGBA")
+            .resize((395, 405), keep_ratio=True)
+            .perspective(points)
+        )
+        return frame.copy().paste(screen.rotate(0, expand=True), (0, 0), below=True)
+
+    return make_jpg_or_gif(images, make)
+
+
+add_meme(
+    "saki_like",
+    saki_like,
+    min_images=1,
+    max_images=1,
+    min_texts=0,
+    max_texts=1,
+    default_texts=[default_text],
+    keywords=["小祥喜欢", "丰川祥子喜欢", "祥子喜欢"],
+    date_created=datetime(2025, 10, 5),
+    date_modified=datetime(2025, 10, 5),
+)
