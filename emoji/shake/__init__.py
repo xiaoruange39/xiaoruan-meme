b3472806fb437be5b1655e0d09563a072712ec0f
xiaoruange39 <2234884717@qq.com>
2025-10-17 10:54:02 +0800

    ➕添加新表情：纯牛马
    ➕添加新表情：摇一摇/晃一晃/摇晃/晃动

---
+++ emoji/shake/__init__.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+
+img_dir = Path(__file__).parent / "images"
+
+
+def shake(images: list[BuildImage], texts, args):
+    img = images[0].convert("RGBA").square().resize((946, 946))
+    frame = BuildImage.open(img_dir / "0.png")
+    frame.paste(img, (47, 47), below=True)
+    return frame.save_jpg()
+
+
+add_meme(
+    "shake",
+    shake,
+    min_images=1,
+    max_images=1,
+    keywords=["摇一摇","晃一晃","摇晃"],
+    date_created=datetime(2025, 10, 17),
+    date_modified=datetime(2025, 10, 17),
+)
